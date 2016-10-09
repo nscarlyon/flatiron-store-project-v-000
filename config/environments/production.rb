c731b174aa83553530031bea0e59b67ed86eea2a
nscarlyon <natasha.carlyon@gmail.com>
2016-10-08 20:29:33 -0400

    remove GA tracker

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  GA.tracker = Rails.application.secrets.google_analytics_code
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
